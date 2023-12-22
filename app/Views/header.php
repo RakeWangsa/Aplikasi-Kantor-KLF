92111e0de5f75890290880001abd785ef44d01c1
Rake Wangsa <rakekw28@gmail.com>
2023-12-22 23:50:05 +0700

    commit 103

---
+++ app/Views/header.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+  <title>KLF - Dashboard</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+<link rel="stylesheet" href="<?= base_url('assets2/style.css'); ?>">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
+
+</head>
+<body>
+<style>
+                    .dashboard-box:hover {
+                        background-color: #f9e8b2;
+                        /* Warna latar belakang saat hover */
+                        border-width: 2px;
+                        /* Lebar border saat hover */
+                    }
+
+                    .dashboard-box {
+                        transition: background-color 0.3s, border-width 0.1s;
+                        /* Efek transisi saat hover */
+                        background-color: #56564C;
+                        /* Warna latar belakang putih */
+                        border: 1.5px solid #ECE5D3;
+                        /* Border abu-abu */
+                        padding: 28px;
+                        border-radius: 8px;
+                        display: flex;
+                        flex-direction: column;
+                        align-items: flex-start;
+                    }
+
+                    .dashboard-box-icon {
+                        font-size: 24px;
+                        color: #cfced2;
+                        padding-right: 8px;
+                    }
+
+                    .dashboard-box-label {
+                        font-size: 16px;
+                        margin-top: 12px;
+                        margin-bottom: 4px;
+                    }
+
+                    .dashboard-box-value {
+                        font-size: 24px;
+                        font-weight: bold;
+                        margin-bottom: 0px;
+                        /* Atur jarak bawah nilai */
+
+                    }
+                </style>
+<!-- partial:index.partial.html -->
+<div class="app-container">
+  <div class="sidebar">
+    <div class="sidebar-header">
+      <div class="app-icon">
+        <!-- <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M507.606 371.054a187.217 187.217 0 00-23.051-19.606c-17.316 19.999-37.648 36.808-60.572 50.041-35.508 20.505-75.893 31.452-116.875 31.711 21.762 8.776 45.224 13.38 69.396 13.38 49.524 0 96.084-19.286 131.103-54.305a15 15 0 004.394-10.606 15.028 15.028 0 00-4.395-10.615zM27.445 351.448a187.392 187.392 0 00-23.051 19.606C1.581 373.868 0 377.691 0 381.669s1.581 7.793 4.394 10.606c35.019 35.019 81.579 54.305 131.103 54.305 24.172 0 47.634-4.604 69.396-13.38-40.985-.259-81.367-11.206-116.879-31.713-22.922-13.231-43.254-30.04-60.569-50.039zM103.015 375.508c24.937 14.4 53.928 24.056 84.837 26.854-53.409-29.561-82.274-70.602-95.861-94.135-14.942-25.878-25.041-53.917-30.063-83.421-14.921.64-29.775 2.868-44.227 6.709-6.6 1.576-11.507 7.517-11.507 14.599 0 1.312.172 2.618.512 3.885 15.32 57.142 52.726 100.35 96.309 125.509zM324.148 402.362c30.908-2.799 59.9-12.454 84.837-26.854 43.583-25.159 80.989-68.367 96.31-125.508.34-1.267.512-2.573.512-3.885 0-7.082-4.907-13.023-11.507-14.599-14.452-3.841-29.306-6.07-44.227-6.709-5.022 29.504-15.121 57.543-30.063 83.421-13.588 23.533-42.419 64.554-95.862 94.134zM187.301 366.948c-15.157-24.483-38.696-71.48-38.696-135.903 0-32.646 6.043-64.401 17.945-94.529-16.394-9.351-33.972-16.623-52.273-21.525-8.004-2.142-16.225 2.604-18.37 10.605-16.372 61.078-4.825 121.063 22.064 167.631 16.325 28.275 39.769 54.111 69.33 73.721zM324.684 366.957c29.568-19.611 53.017-45.451 69.344-73.73 26.889-46.569 38.436-106.553 22.064-167.631-2.145-8.001-10.366-12.748-18.37-10.605-18.304 4.902-35.883 12.176-52.279 21.529 11.9 30.126 17.943 61.88 17.943 94.525.001 64.478-23.58 111.488-38.702 135.912zM266.606 69.813c-2.813-2.813-6.637-4.394-10.615-4.394a15 15 0 00-10.606 4.394c-39.289 39.289-66.78 96.005-66.78 161.231 0 65.256 27.522 121.974 66.78 161.231 2.813 2.813 6.637 4.394 10.615 4.394s7.793-1.581 10.606-4.394c39.248-39.247 66.78-95.96 66.78-161.231.001-65.256-27.511-121.964-66.78-161.231z"/></svg> -->
+        <img src="<?= base_url('assets2/klf-logo.png') ?>" alt="Logo" width="320px" height="100px" style="margin-left:-73px">
+        <!-- <p class="text-light">Karya Logam Furniture</p> -->
+      </div>
+      
+    </div>
+    <ul class="sidebar-list">
+      <li class="sidebar-list-item active">
+        <a href="<?= base_url('dashboard'); ?>">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-dashboard">
+    <rect x="3" y="3" width="6" height="6" />
+    <rect x="15" y="3" width="6" height="6" />
+    <rect x="3" y="15" width="6" height="6" />
+    <rect x="15" y="15" width="6" height="6" />
+</svg>
+
+          <span>Dashboard</span>
+        </a>
+      </li>
+      <li class="sidebar-list-item">
+        <a href="<?= base_url('order/listOrder'); ?>">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-list-order">
+    <line x1="5" y1="6" x2="19" y2="6" />
+    <line x1="5" y1="12" x2="19" y2="12" />
+    <line x1="5" y1="18" x2="19" y2="18" />
+</svg>
+
+
+          <span>List Order</span>
+        </a>
+      </li>
+
+      <li class="sidebar-list-item">
+        <a href="<?= base_url('taskCalendar'); ?>">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-task-calendar">
+    <rect x="3" y="4" width="18" height="18" rx="2" ry="2" />
+    <line x1="16" y1="2" x2="16" y2="6" />
+    <line x1="8" y1="2" x2="8" y2="6" />
+    <line x1="3" y1="10" x2="21" y2="10" />
+    <line x1="3" y1="14" x2="21" y2="14" />
+</svg>
+
+          <span>Task Calendar</span>
+        </a>
+      </li>
+      <li class="sidebar-list-item">
+        <a href="<?= base_url('kategoriProduk'); ?>">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+  <path d="M5 4h14c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H5c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2zm0 5h14M5 14h14M5 9h14"></path>
+</svg>
+          <span>Kategori Produk</span>
+        </a>
+      </li>
+      <li class="sidebar-list-item">
+        <a href="<?= base_url('supplier'); ?>">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-supplier">
+    <rect x="3" y="8" width="18" height="12" rx="2" ry="2" />
+    <rect x="8" y="6" width="8" height="4" rx="1" ry="1" />
+    <path d="M12 16v3m-2-3v3m4-3v3" />
+</svg>
+
+          <span>Supplier</span>
+        </a>
+      </li>
+      <li class="sidebar-list-item">
+        <a href="<?= base_url('katalogProduk'); ?>">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-product-catalog">
+    <rect x="2" y="3" width="20" height="18" rx="2" ry="2" />
+    <line x1="9" y1="7" x2="15" y2="7" />
+    <line x1="9" y1="11" x2="15" y2="11" />
+    <line x1="9" y1="15" x2="13" y2="15" />
+</svg>
+
+
+
+
+          <span>Katalog Produk</span>
+        </a>
+      </li>
+    </ul>
+    <div class="account-info">
+      <div class="account-info-picture">
+        <img src="https://images.unsplash.com/photo-1527736947477-2790e28f3443?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTE2fHx3b21hbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60" alt="Account">
+      </div>
+      <div class="account-info-name"><?= session()->get('name') ?></div>
+      <a class="account-info-more" href="<?= base_url('logout'); ?>">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-out">
+        <path d="M19 12H5M12 19l7-7-7-7"/>
+    </svg>
+                  </a>
+
+
+
+    </div>
+  </div>
